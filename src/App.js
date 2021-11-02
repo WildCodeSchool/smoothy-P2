a23089da62d85a0b585c60b7dd83d686afdfd4d3
stvial <stanislas.vidal@gmail.com>
2021-11-02 16:07:53 +0100

    Add route structure in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import HomePage from './Components/Navbar/NavComponents/HomePage'
+import Navbar from './Components/Navbar/Navbar'
+import Barcode from "./Components/Navbar/NavComponents/Barcode";
+import SearchFunc from "./Components/Search/SearchFunc";
 const App = () => {
-  return <>Just a component</>;
-};
+  return (
+
+    <Router>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path='/barcode'>
+        <Barcode/>
+        </Route>
+        </Switch>
+        </div>
+
+    </Router>
+    
+  );
+}
 
 export default App;
