6e815d9a4707aa453941e670f8b0f48dcbf34c67
Stanislas Vidal <79643611+stvidal@users.noreply.github.com>
2021-11-08 17:28:13 +0100

    Create ProductList.jsx

---
+++ src/Components/ProductList/ProductList.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './PageListSetter.css'
+import moreProducts from  '../../Assets/moreProducts.png'
+
+function PageListSetter({page, setPage,produits}) {
+
+    const handleClick = () => {
+        
+    setPage(++page)
+
+    console.log(produits)
+
+}
+    return (
+      
+        <div className='more-button'>
+            <img  className={/\d/.test(produits)
+         ? 'hidden-button-style'
+         : 'more-button-style'} 
+         onClick={handleClick} 
+         src={moreProducts} alt="more-products" />
+        </div>
+
+       
+    )
+}
+
+export default PageListSetter
