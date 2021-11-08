11b6ab18a3cd84f8161f8021e3bce85891d4f69c
Vincent Doree <vinz77@hotmail.com>
2021-11-08 11:12:44 +0100

    Layout SearchProductList

--- src/Components/Search/SearchProductList.jsx
+++ src/Components/Search/SearchProductList.jsx
@@ -6,15 +6,21 @@ function SearchProductList({ products }) {
       {products && (
         <div className="map-products">
           {products.map((product) => (
-            <p className="items" key={product.id}>
-              Marque :{product.brands}
+            <div className="items" key={product.id}>
+              <div className="items1" >
               <img
                 className="images"
                 key={product.id}
                 src={product.image_front_small_url}
                 alt={""}
               />
-            </p>
+              </div>
+              <p className='pp'>{product.brands}
+              </p><div className='ppppp'><div className ='ppp'>NutriScore</div> 
+              <div className ='pppp'>Label</div>
+              </div>
+               
+            </div>
           ))}
         </div>
       )}
