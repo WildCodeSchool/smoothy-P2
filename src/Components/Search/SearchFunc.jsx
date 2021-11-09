bf5e5ee31e9436ae3b20199a5b5a12ff985929e8
Stanislas Vidal <79643611+stvidal@users.noreply.github.com>
2021-11-09 02:21:10 +0100

    Update SearchFunc.jsx

--- src/Components/Search/SearchFunc.jsx
+++ src/Components/Search/SearchFunc.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 import SearchProductList from "./SearchProductList.jsx";
 import './SearchFunc.css'
 import PageListSetter from '../ProductList/PageListSetter.jsx'
-
 let url = "";
 
 const SearchFunc = ({produits}) => {
@@ -11,11 +10,16 @@ const SearchFunc = ({produits}) => {
  
   const [products, setProducts] = useState(null);
   const [page, setPage] = useState(1);
+  
+
 
 useEffect(() => {
+
+  
   /\d/.test(produits)
   ? url = `https://fr.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=_id&tag_contains_0=contains&tag_0=${produits}&fields=code,product_name_fr,brands_tags,image_front_small_url,quantity,nutriscore_grade,labels_old,brand,_keywords&json=true` 
-  : url = `https://fr.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=_keywords&tag_contains_0=contains&tag_0=${produits}&fields=code,product_name_fr,brands_tags,image_front_small_url,quantity,nutriscore_grade,labels_old,brands,_keywords&page_size=5&page=${page}&json=true`
+
+  : url = `https://fr.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=_keywords&tag_contains_0=contains&tag_0=${produits}&fields=code,product_name_fr,brands_tags,image_front_small_url,quantity,nutriscore_grade,labels_old,brands,_keywords&page_size=24&page=${page}&json=true`
   
   const getProducts = async () => {
     await axios
@@ -27,6 +31,10 @@ useEffect(() => {
   
 }, [produits, page])
 
+useEffect(() => {
+    setPage(1)
+}, [produits])
+
   return (
     <div className="Container">
 
@@ -34,9 +42,8 @@ useEffect(() => {
     </div>
       <SearchProductList products={products}/>
       <PageListSetter page={page} setPage={setPage} produits={produits}/>
-
     </div>
   );
 };
 
-export default SearchFunc;   
+export default SearchFunc;    
